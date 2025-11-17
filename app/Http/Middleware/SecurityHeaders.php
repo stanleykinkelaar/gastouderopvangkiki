92359eed103ad8f4525304b8d90286b51ca95fca
Stanley Kinkelaar <stanley@stanleykinkelaar.nl>
2025-11-17 17:15:02 +0100

    Update CSP in `SecurityHeaders` middleware to allow `'unsafe-eval'` for scripts

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -34,7 +34,7 @@ class SecurityHeaders
         } else {
             $csp = implode('; ', [
                 "default-src 'self'",
-                "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://challenges.cloudflare.com",
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://challenges.cloudflare.com",
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com",
                 "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com",
                 "img-src 'self' data: https: https://picsum.photos",
